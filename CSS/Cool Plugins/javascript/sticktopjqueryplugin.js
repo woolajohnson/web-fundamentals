55fbb21415132484837ffade916b61a23e0ce526
Woola Johnson <woolajohnson@gmail.com>
2024-01-19 15:48:49 +0800

    Done Cool Plugins

---
+++ CSS/Cool Plugins/javascript/sticktopjqueryplugin.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+            $("header").addClass("scrolled");
+        } else {
+            $("header").removeClass("scrolled");
+        }
+    });
+});
